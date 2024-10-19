f1aa723df834c76bf8bc3203406bb6e3e78b6418
DuertaMar <166946889+DuertaMar@users.noreply.github.com>
2024-10-19 16:02:48 -0300

    Update PokemonLegends.java

---
+++ PokemonLegends.java
@@ -0,0 +1,26 @@
+public class PokemonLegends extends Pokemon  {
+    private String habitat;
+    private String raridade;
+
+    public PokemonLegends(String nome, String t, String n, String h, String e, String m, String st, int[] IVs,String habitate, String raridadee) {
+        super(nome, t, n, h, e, m, st, IVs);
+        this.setHabitat(habitate);
+        this.setRaridade(raridadee);
+
+    }
+    public String getHabitat() {
+        return habitat;
+    }
+    public void setHabitat(String habitate) {
+        this.habitat=habitate;
+    }
+    public String getRaridade() {
+        return raridade;
+    }
+    public void setRaridade(String raridade1) {
+        this.raridade=raridade1;
+    }
+    public void descreverHabitat () {
+        System.out.println("O local que este pokemon habita é :"+this.habitat);
+    }
+}
